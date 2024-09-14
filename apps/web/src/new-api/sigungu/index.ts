86dad2020956056f486913d212b1ff08e917c26e
woohyunKim <whkim@kephas.kr>
2024-09-15 02:46:51 +0900

    🚩 api 작업 수정 / 타입 오류로 인해 변경 진행

--- apps/web/src/new-api/sigungu/index.ts
+++ apps/web/src/new-api/sigungu/index.ts
@@ -1,5 +1,5 @@
 import { SigunguRequestType } from "front/new-types/requestAPI";
 import { getAPI } from "../service";
-import { SigunguResponse } from "front/new-types/responseAPI";
+import { Sigungu } from "front/new-types/responseAPI";
 
-export const getSigungu = (props : SigunguRequestType) => getAPI<SigunguRequestType, SigunguResponse>(props, '/sigungu')
+export const getSigungu = (props : SigunguRequestType) => getAPI<SigunguRequestType, Sigungu>(props, '/sigungu')
