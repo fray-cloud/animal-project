12e8f00db76f3071b7c61796fea50131450f476a
woohyunKim <whkim@kephas.kr>
2024-09-18 19:18:12 +0900

    🚩 form 핸들러 내 request 정보 받을 수 있도록 함 / serchView 추가

--- apps/web/src/new-site/search/search.tsx
+++ apps/web/src/new-site/search/search.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { SearchForm } from './Form';
+import { SearchView } from './SearchView';
+import { AnimalInfoRequestType } from 'front/new-types/requestAPI';
+import { useAnimalInfo } from 'front/hooks';
 
 export const SearchPage = () => {
+  const [animalInfoRequest, SetAnimalInfoRequest] =
+    useState<AnimalInfoRequestType | null>(null);
+
   return (
     <>
-      <SearchForm />
+      <SearchForm
+        submitHandler={(data) => {
+          SetAnimalInfoRequest(data);
+        }}
+      />
+      <SearchView animalInfoRequest={animalInfoRequest} />
     </>
   );
 };
