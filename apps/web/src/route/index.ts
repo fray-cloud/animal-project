aaa8e59acafc0d30ae471221221a2f9aeed588f2
woohyunKim <whkim@kephas.kr>
2024-09-19 20:32:54 +0900

    🐛 router 구조 변경

--- apps/web/src/route/index.ts
+++ apps/web/src/route/index.ts
@@ -1,12 +1,14 @@
 import SearchRouter from 'front/new-site/search';
 import React from 'react';
 import { useRoutes, RouteObject } from 'react-router-dom';
+import HomePage from 'front/new-site/home'
 const SiteRouter = () => {
   const router: RouteObject[] = [
     {
       path: '/',
-      children: [...SearchRouter],
+      Component: HomePage,
     },
+    ...SearchRouter,
   ];
   return useRoutes(router);
 };
