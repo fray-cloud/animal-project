67363b258b904c0208003a3b33707861723a128a
woohyunKim <whkim@kephas.kr>
2024-09-20 21:02:12 +0900

    🚩 광역시 현황 배너 기능 추가

--- apps/web/src/new-site/home/Count.tsx
+++ apps/web/src/new-site/home/Count.tsx
@@ -2,7 +2,8 @@ import { useAnimalInfoSidoCount } from 'front/hooks';
 import { v4 as uuidv4 } from 'uuid';
 import React from 'react';
 import { Sido } from 'front/new-types/responseAPI';
-import './scroll.scss';
+import InfiniteLoopSlider from './InfiniteLoop';
+import './infinity.scss';
 
 type Props = {
   items: Sido[];
@@ -13,12 +14,12 @@ export const CountList = (props: Props) => {
   const query = useAnimalInfoSidoCount(items);
 
   return (
-    <div className="w-[95vw] flex snap-x snap-mandatory overflow-x-auto">
-      <div className="flex gap-6 animate-scroll">
+    <>
+      <InfiniteLoopSlider onHoverStop>
         {query?.map((result) => {
           if (result.data?.sido.orgCd === '') return null;
           return (
-            <div className="snap-start shrink-0">
+            <div className="shrink-0">
               <div className="flex items-center gap-3">
                 <div className="avatar">
                   <div
@@ -56,49 +57,7 @@ export const CountList = (props: Props) => {
             </div>
           );
         })}
-
-        {query?.map((result) => {
-          if (result.data?.sido.orgCd === '') return null;
-          return (
-            <div aria-hidden className="snap-start shrink-0">
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div
-                    className={`w-7 rounded ${
-                      result.isLoading ? 'skeleton' : ''
-                    }`}
-                  >
-                    {result.isLoading ? null : (
-                      <img
-                        src={`${import.meta.env.BASE_URL}logo/${
-                          result.data?.sido.orgCd
-                        }.png`}
-                        alt="logo"
-                      />
-                    )}
-                  </div>
-                </div>
-                <div>
-                  <div
-                    className={`font-bold ${
-                      result.isLoading ? 'skeleton w-full' : ''
-                    }`}
-                  >
-                    {result.isLoading ? null : result.data?.sido.orgdownNm}
-                  </div>
-                  <div
-                    className={`text-sm opacity-50 ${
-                      result.isLoading ? 'skeleton w-full' : ''
-                    }`}
-                  >
-                    {result.isLoading ? null : result.data?.totalCount}
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+      </InfiniteLoopSlider>
+    </>
   );
 };
