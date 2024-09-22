969de4b319d8ab7d6df31668efb5d6914ed6f7b7
woohyunKim <whkim@kephas.kr>
2024-09-23 02:52:05 +0900

    ⚰️ sidebar 사용 안함으로 삭제

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,26 +1,16 @@
-import React, { useState } from 'react';
-import Sidebar from 'front/new-component/sidebar';
+import React from 'react';
 import Header from 'front/new-component/header';
 
 import 'front/app/app.module.scss';
 import SiteRouter from 'front/route';
-import { Outlet, Route, Routes } from 'react-router-dom';
-import SearchRouter from 'front/new-site/search';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Bottom } from 'front/new-component';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
   return (
     <div className="flex max-h-screen max-w-screen">
-      {/* <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} /> */}
       <div className="flex-1 flex flex-col">
         <Header />
         <main className="p-4 flex-1">{children}</main>
