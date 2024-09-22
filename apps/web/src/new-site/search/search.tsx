3c631b67907f9cff81a4dd1a275789106323f091
woohyunKim <whkim@kephas.kr>
2024-09-23 02:58:20 +0900

    💄 스크롤 애니매니션 추가

--- apps/web/src/new-site/search/search.tsx
+++ apps/web/src/new-site/search/search.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { SearchForm } from './Form';
 import { SearchView } from './SearchView';
 import { AnimalInfoRequestType } from 'front/new-types/requestAPI';
+import { motion, useScroll } from 'framer-motion';
 
 export const SearchPage = () => {
   const [animalInfoRequest, SetAnimalInfoRequest] =
     useState<AnimalInfoRequestType | null>(null);
+
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({ container: scrollRef });
 
   return (
     <>
@@ -17,7 +21,14 @@ export const SearchPage = () => {
             }}
           />
         </div>
-        <div className="col-span-full h-[60vh] overflow-auto">
+        <motion.div
+          className="col-span-full top-0 left-0 right-0 h-1 bg-green-500 origin-left"
+          style={{ scaleX: scrollYProgress }}
+        />
+        <div
+          className="col-span-full h-[60vh] overflow-auto scrollbar-hide"
+          ref={scrollRef}
+        >
           <SearchView animalInfoRequest={animalInfoRequest} />
         </div>
       </div>
