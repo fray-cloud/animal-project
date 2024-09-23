cba1e10a1b78341959e5b49b3d07b0b899c5c813
woohyunKim <whkim@kephas.kr>
2024-09-23 23:22:11 +0900

    💄 좋아요 버튼 추가

--- apps/web/src/new-site/search/card/AnimalCard.tsx
+++ apps/web/src/new-site/search/card/AnimalCard.tsx
@@ -1,5 +1,7 @@
 import { AnimalInfo } from 'front/new-types/responseAPI';
-import React from 'react';
+import React, { useState } from 'react';
+import { FaHeart } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 type Props = {
   item: AnimalInfo;
@@ -7,8 +9,17 @@ type Props = {
 
 export const AnimalCard = (props: Props) => {
   const { item } = props;
+  const [isLiked, setIsLiked] = useState(false);
   return (
     <div className="card bg-base-100 shadow-xl">
+      <div className="absolute top-3 right-3">
+        <motion.div
+          whileTap={{ scale: 1.3 }}
+          onClick={() => setIsLiked(!isLiked)}
+        >
+          <FaHeart className={`${isLiked ? 'text-red-500' : ''}`} />
+        </motion.div>
+      </div>
       <figure className="px-10 pt-10">
         <img src={item.popfile} alt="pet" className="rounded-xl h-32" />
       </figure>
