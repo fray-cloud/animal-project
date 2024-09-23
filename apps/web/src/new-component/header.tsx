1b231d971c8fee11fa5723349bbf7a9f31ed967d
woohyunKim <whkim@kephas.kr>
2024-09-24 02:04:15 +0900

    v0.0.3.1

--- apps/web/src/new-component/header.tsx
+++ apps/web/src/new-component/header.tsx
@@ -11,26 +11,11 @@ const Header: React.FC = () => {
     <div className="navbar bg-neutral text-neutral-content">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl" onClick={() => navigate('/')}>
-          유기동물 조회 시스템
+          유기동물 조회 서비스
         </a>
       </div>
       <div className="flex-none">
         <CountList items={data} />
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-5 w-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
       </div>
     </div>
   );
